ff88dc67b4a138267f43da84059ef852c47f703e
dudy2017 <peterdud@gmail.com>
2018-02-19 19:33:09 +0100

    input mező és képek elhelyezése

--- src/app/listdemo/listdemo.component.ts
+++ src/app/listdemo/listdemo.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 export class EventModel {
   id: number;
   name: string;
+  pic: string;
 }
 
 @Component({
@@ -18,19 +19,24 @@ export class ListdemoComponent {
     this.events = [
       {
         id: 1,
-        name: 'sziget'
+        name: 'Sziget fesztivál',
+        pic: 'https://bkk.hu/wp-content/uploads/2017/08/sziget-2017-logo.png'
       },
       {
         id: 2,
-        name: 'fezen'
+        name: 'fezen',
+        pic: 'http://hammerworld.hu/images/upload/FEZ17WHITE.jpg'
       },
       {
         id: 3,
-        name: 'rockmaraton'
+        name: 'rockmaraton',
+        pic: 'https://metal.hu/wp-content/uploads/2017/07/15327345_10155464553766521_811722810673252682_n.png'
+
       },
         {
         id: 4,
-        name: 'oktobefest'
+        name: 'oktobefest',
+        pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3dPHcxgo2BiHixXMOGb1A21MEsxKQGsUUTwfGx68pi2rC1dRq'
       }
     ];
     console.log('megjött a listdemo');
@@ -39,4 +45,11 @@ export class ListdemoComponent {
   delete(id: number) {
     this.events = this.events.filter( (ev: EventModel) => ev.id !== id   );
   }
+
+  add(newEventNameInput: HTMLInputElement) {
+    console.log(newEventNameInput.value);
+    newEventNameInput.value = '';
+  }
+
+
 }
