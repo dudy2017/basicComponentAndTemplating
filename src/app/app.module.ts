f822e5b0c9027b28dc47aa15433e8406081ef3b3
dudy2017 <peterdud@gmail.com>
2018-02-19 17:59:47 +0100

    listdemo component létrehozása az eddigi basicComponentAndTemplating-ből

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { EventComponent } from './event/event.component';
 import {FormsModule} from '@angular/forms';
+import { ListdemoComponent } from './listdemo/listdemo.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventComponent
+    EventComponent,
+    ListdemoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/listdemo/listdemo.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listdemo',
+  templateUrl: './listdemo.component.html',
+  styleUrls: ['./listdemo.component.css']
+})
+export class ListdemoComponent {
+
+  constructor() {
+    console.log('megjött a listdemo');
+  }
+
+}
